cd8b2809d482fe812782885d39a57d25afa0e2ab
Tronixel75 <pavelporoshin3@gmail.com>
2024-06-16 21:09:07 +0300

    Initial commit

---
+++ Source/SnakeGame/MySnakeGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SnakeGameGameModeBase.h"
+#include "MySnakeGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SNAKEGAME_API AMySnakeGameGameModeBase : public ASnakeGameGameModeBase
+{
+	GENERATED_BODY()
+	
+};
